12a15286c2fa45ac8fe6426f8875975c9bef8146
mateeda <136304651+mateeda@users.noreply.github.com>
2023-07-24 13:26:49 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,20 @@
+// 変数の宣言
+let userName;
+
+// 値の代入
+userName='体重';
+
+// コンソールへの出力
+console.log(userName);
+
+// 変数の宣言
+let userNumber;
+
+// 値の代入
+userNumber='身長';
+
+// コンソールへの出力
+console.log(userNumber);
+
+// 計算式
+console.log(68/(1.7*1.7));
