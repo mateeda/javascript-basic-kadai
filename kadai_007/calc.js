93b0d1bd15463da4ca27981a88fc06d80dbdafa1
mateeda <136304651+mateeda@users.noreply.github.com>
2023-07-24 14:29:12 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,18 +1,13 @@
-num=Math.floor(15);
+let num=25;
 
-console.log(num % 15==0);
+console.log(num);
 
-switch(num){
-    case 6:
+if((num %3===0)&&(num %5===0)){
+    console.log('3と5の倍数')
+}
+else if(num %3===0){
         console.log('3の倍数です');
-        break;
-    case 10:
+}
+else if(num %5===0){
         console.log('5の倍数です');
-        break;
-    case 15:
-        console.log('3と5の倍数です');
-        break;
-    default:
-        console.log(num);
-        break;
 }
