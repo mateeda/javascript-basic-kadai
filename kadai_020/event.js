fe5c8306ae80e9af3c9425d813e0ab4f2cd46148
mateeda <136304651+mateeda@users.noreply.github.com>
2023-08-01 15:19:27 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,10 +1,19 @@
 // btnというidを持つHTML要素を取得し定数に代入する
 const btn = document.getElementById('btn');
 
+// btnというidを持つHTML要素を取得し定数に代入する
+const text = document.getElementById('text');
+
+
 // HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click',()=>{
     // 2秒の待ち時間を設定しtextの変更を実行する
     setTimeout(()=>{
-        text.textContent='ボタンをクリックしました';
+    
+    //textの設定とクリックされたときにイベント処理を実行する
+    text.textContent=('ボタンをクリックしました');
+
     },2000);
 })
+
+
